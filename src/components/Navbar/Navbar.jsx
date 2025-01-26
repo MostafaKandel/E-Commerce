57fe89d895fe41317041ab250620dcef2bbfaddb
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-01-26 10:42:50 +0100

    feat: initial implementation of Navbar Component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import {
+  Navbar as HerouiNavbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Link,
+  Button,
+} from "@heroui/react";
+
+
+export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    
+  ];
+
+  return (
+    <HerouiNavbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+           
+          <p className="font-bold text-inherit">ACME</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+      {menuItems.map((item, index) => (
+            <NavbarItem key={index}>
+            <Link color="foreground" href="#">
+              {item}
+            </Link>
+          </NavbarItem>
+        ))}
+        
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem  className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className="w-full"
+              color={
+                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
+              }
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </HerouiNavbar>
+  );
+}
