22de109ff970f47887115e740b49d0c3a49698db
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-02-16 11:09:34 +0100

    fix: add state of current user data

--- src/Contexts/AuthContext.jsx
+++ src/Contexts/AuthContext.jsx
@@ -7,6 +7,8 @@ export const authContext = createContext();
 export default function AuthContextProvider({ children }) {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
+    const [userId, setUserId] = useState(null);
+    const [userData, setUserData]= useState()
      
     useEffect(() => {
         verifyUserToken();
@@ -19,11 +21,14 @@ export default function AuthContextProvider({ children }) {
                 token: localStorage.getItem('token')
             }
         }).then((res) => {
-            setIsLoggedIn(true);    
+            setIsLoggedIn(true);
+            setUserData(res.data.decoded) 
+            setUserId(res.data.decoded.id);
+               
         }).catch((err) => {
             localStorage.removeItem('token');
             setIsLoggedIn(false);
         }).finally(() => setIsLoading(false));
     }
-    return <authContext.Provider value={{ isLoggedIn, setIsLoggedIn, isLoading }}>{children}</authContext.Provider>;
+    return <authContext.Provider value={{ isLoggedIn, setIsLoggedIn, isLoading, userId, userData }}>{children}</authContext.Provider>;
 }
